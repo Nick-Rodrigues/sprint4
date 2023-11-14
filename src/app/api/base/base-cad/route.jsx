450f1b209302dc1c14e727922d7eca96faa867ea
DennerDuarte <rm551938@fiap.com.br>
2023-11-13 23:30:49 -0300

    Criado o POST da API completo

--- src/app/api/base/base-cad/route.jsx
+++ src/app/api/base/base-cad/route.jsx
@@ -1,3 +1,26 @@
 export async function POST(request, response){
+
+
+        const userRequest = await request.json();
+        
+        const file = await fs.readFile(
+            process.cwd() + "/src/apop/api/base/db.json","utf8"
+        );
+            
+        const lista = await JSON.parse(file);
+            
+        const newId = lista.usuarios[lista.usuarios.length - 1].id +1;
+            
+        userRequest.id = newId;
+            
+        lista.usuarios.push(userRequest);
+            
+        await fs.writeFile(
+            process.cwd() + "/src/app/api/base/db.json",
+            JSON.stringify(lista)
+        );
+            
+        return NextResponse.json(userRequest);
+        
     
 }
