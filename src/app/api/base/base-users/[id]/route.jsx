172834e40f603d926cf232e4340702434dd8e496
DennerDuarte <rm551938@fiap.com.br>
2023-11-13 23:34:20 -0300

    Criada a estrutura do GET completa e iniciada o POST

--- src/app/api/base/base-users/[id]/route.jsx
+++ src/app/api/base/base-users/[id]/route.jsx
@@ -1,3 +1,28 @@
+import {promises as fs} from "fs";
+import { NextResponse } from "next/server";
+
 export async function GET(request,{params}){
+    const file = await fs.readFile(process.cwd() + "/src/app/api/base/db.json", 'utf8');
+
+    const lista = await JSON.parse(file);
+
+    const id = params.id;
+
+    if(id > 0 && id <= lista.usuarios.length){
+        return NextResponse.json(lista.usuarios.find((user)=> user.id == id));
+    } else {
+        return id == 0 ? NextResponse.json(lista.usuarios) : NextResponse.redirect("http://localhost:3000/error")
+    }
+}
+
+export async function POST(request, response){
+  
+    const file = await fs.readFile(process.cwd() + "/src/app/api/base/db.json", 'utf8');
+  
+    const lista = await JSON.parse(file);
+  
+    const userRequest = await request.json();
+  
     
+  
   }
