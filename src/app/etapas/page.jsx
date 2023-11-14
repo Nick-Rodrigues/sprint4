8121571bbae7e53dbddece96621f82d874a9942b
ThiagoAgostinho <thiagor793@gmail.com>
2023-11-13 23:33:28 -0300

    Criada Div Cadastramento com o Bot.

---
+++ src/app/etapas/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function etapas() {
+  return (
+    <>
+    <div className="etapas-main">
+        <h1 className="caixaAlta">Etapa do asseguramento</h1>
+            <div>
+                <h2 className="titulo-etapas">Cadastramento com o bot</h2>
+                    <p className="texto-etapas">
+                        Com nosso bot de cadastros é muito mais facil e rapido, pois será
+                        solicitado os dados e fotos necessárias, e logo em seguida com nosso
+                        processo de avaliação já tera a sua resposta se seu seguro foi a
+                        aprovado ou não.
+                    </p>
+            </div>
+            
+
+    </div>
+    </>
+  )
+  }
